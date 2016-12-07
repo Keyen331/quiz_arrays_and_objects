fcbba45b09a3cc706790cbc0048826205dd11682
Keyen331 <banayos.kay@gmail.com>
2016-12-07 08:33:24 -1000

    uploading

---
+++ quiz.js
@@ -0,0 +1,33 @@
+/*
+
+  Kay Hian Calpito
+  Period 1
+  Dec, 7, 2016
+
+*/
+
+var myArray = ["love", "hatred", 10, true];
+console.log(myArray[2]);
+
+function me (x) {
+  if (myArray[3] == true) {
+    console.log(myArray[0] + " and " + myArray[1] + " is in level " + myArray[2] + " inside me right now.");
+  }else {
+    console.log("You shall not pass!");
+  }
+}
+
+me("shhhh")
+
+var batman = {
+  Man : true,
+  utilityBelt : ["shutgon", "samurai", "wand"],
+  vehicle : "batmobile",
+  sidekick :"robin"
+}
+
+function bat(one){
+  if (batman.man == true) {
+    console.log("My partner " + batman.sidekick + " used" + )
+  }
+}
